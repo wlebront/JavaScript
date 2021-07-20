d1585b59f15489ccc980f56ca1e56dd562560804
William Lebron <WilliamLebron23@hotmail.com>
2021-07-19 20:00:55 -0400

    JavaScript Variables

---
+++ js/01.js
@@ -0,0 +1,25 @@
+// Variables 
+
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+var disponible; // iniciamos la variable pero sin valor.
+
+producto = true; // Reasignando el valor de la variable
+
+disponible = true
+
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// var 1disponible
+var disponible_;
+
+// Estilos para las variables
+var nombre_producto = 'Monitor HD'; // underscore
+var nombreProducto = 'Monitor HD'; // Camecase
+var NombreProducto = 'Monitor HD'; // Pascal Case
+var nombreproducto = 'Monitor HD'; 
+
+console.log(nombre_producto);
